871b6c122734ecfa7acea3e6c2e817b12a39c8d4
Daniel Santos <danieldiefsantos@gmail.com>
2025-03-03 16:42:00 -0300

    feat: arquivo principal para execução da API

---
+++ server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import { getPartnerSales } from "./routes/reports/partnerSales";
+import { getPaymentMethods } from "./routes/fixeds";
+import {
+  getShedulesByDate,
+  getCartByLocator,
+  getProductByBarcode,
+  postCheckAccess,
+  postUpdateProduct
+} from "./routes/functions/alterWaveRoutes";
+
+const app = express();
+app.use(cors());
+const port = 3222;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+//-----------------------Rotas-----------------------
+// Relatório de vendas por parceiros
+getPartnerSales(app);
+getPaymentMethods(app);
+
+// Alteração de sessões 
+getShedulesByDate(app);
+getCartByLocator(app);
+getProductByBarcode(app);
+postCheckAccess(app);
+postUpdateProduct(app);
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
