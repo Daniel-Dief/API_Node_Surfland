e1f22b086c3e069b359facdcffe4e3f7e4a934c1
Daniel Santos <danieldiefsantos@gmail.com>
2025-03-03 16:40:23 -0300

    feat: adicionando funções uteis

---
+++ utils.ts
@@ -0,0 +1,41 @@
+import { ParsedQs } from 'qs';
+import mysql from 'mysql2/promise';
+import { dbConfigIntranet } from './dbConnectors';
+import { changeProduct } from './types/log';
+
+function formatarDataSQL(data: string | string[] | Date | ParsedQs | (string | ParsedQs)[] | undefined): string | undefined {
+    if (data) { 
+        const valor = Array.isArray(data) ? data[0] : data;
+        if (valor instanceof Date) {
+            return valor.toISOString().replace('T', ' ').slice(0, -1);
+        }
+        if (typeof valor === 'string') {
+            const d = new Date(valor);
+            return !isNaN(d.getTime()) ? d.toISOString().replace('T', ' ').slice(0, -1) : undefined;
+        }
+    }
+}
+
+// Função para salvar log no Banco de Dados
+async function Log(usrDocument : string, oldProduct : changeProduct, newProduct : changeProduct) {
+    const connection = await mysql.createConnection(dbConfigIntranet);
+    const [rows] = await connection.execute(
+      `SELECT UserId FROM Users WHERE Login = ?`,
+      [usrDocument]
+    );
+    const userId = (rows as any)[0]?.UserId ?? 0;
+    
+    if(userId == 0) {
+      console.log("Erro ao buscar usuário");
+    }
+  
+    const str_oldProduct = JSON.stringify(oldProduct);
+    const str_newProduct = JSON.stringify(newProduct);
+  
+    await connection.execute(
+      `INSERT INTO Logs (UserId, OldJSON, NewJSON) VALUES (?, ?, ?)`,
+      [userId, str_oldProduct, str_newProduct]
+    );
+  }
+
+export { formatarDataSQL, Log };
