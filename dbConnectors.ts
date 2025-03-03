137875ec48948d505ebb8d2bde7b43969b6f02dc
Daniel Santos <danieldiefsantos@gmail.com>
2025-03-03 16:48:23 -0300

    refact: caso .env não tenha sido definid

--- dbConnectors.ts
+++ dbConnectors.ts
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 // Configuração da conexão com o banco de dados
 const dbConfigIntranet = {
-    host: process.env.DB_INTRANET_HOST,
-    user: process.env.DB_INTRANET_USER,
-    password: process.env.DB_INTRANET_PASSWORD,
-    database: process.env.DB_INTRANET_DATABASE,
+    host: process.env.DB_INTRANET_HOST ?? "",
+    user: process.env.DB_INTRANET_USER ?? "",
+    password: process.env.DB_INTRANET_PASSWORD ?? "",
+    database: process.env.DB_INTRANET_DATABASE ?? "",
 }
 
 const dbConfigPortal = {
-    user: process.env.DB_PORTAL_USER,
-    password: process.env.DB_PORTAL_PASSWORD,
-    server: process.env.DB_PORTAL_HOST,
-    database: process.env.DB_PORTAL_DATABASE,
+    user: process.env.DB_PORTAL_USER ?? "",
+    password: process.env.DB_PORTAL_PASSWORD ?? "",
+    server: process.env.DB_PORTAL_HOST ?? "",
+    database: process.env.DB_PORTAL_DATABASE ?? "",
     options: {
         encrypt: true,
         trustServerCertificate: true,
@@ -20,11 +20,11 @@ const dbConfigPortal = {
 };
 
 const dbConfigTravel = {
-    host: process.env.DB_TRAVEL_HOST,
-    port: process.env.DB_TRAVEL_PORT,
-    user: process.env.DB_TRAVEL_USER,
-    password: process.env.DB_TRAVEL_PASSWORD,
-    database: process.env.DB_TRAVEL_DATABASE,
+    host: process.env.DB_TRAVEL_HOST ?? "",
+    port: process.env.DB_TRAVEL_PORT ?? "",
+    user: process.env.DB_TRAVEL_USER ?? "",
+    password: process.env.DB_TRAVEL_PASSWORD ?? "",
+    database: process.env.DB_TRAVEL_DATABASE ?? "",
 }
 
 export { dbConfigIntranet, dbConfigPortal, dbConfigTravel };
