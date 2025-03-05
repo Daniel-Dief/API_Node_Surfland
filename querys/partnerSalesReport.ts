73e676192cc8fcb5de3b9d1892d97acfd61c74cc
Daniel Santos <danieldiefsantos@gmail.com>
2025-03-05 17:20:26 -0300

    refactor: ajuste na query de vendas de parceiros

--- querys/partnerSalesReport.ts
+++ querys/partnerSalesReport.ts
@@ -7,86 +7,104 @@ interface queryPartnersReportProps {
 
 function queryPartnersReport({ paymentMethod, saleDate, saleDateEnd, partnerName }: queryPartnersReportProps) {
     return `
-    SELECT
+      SELECT
         s.SaleId AS "CodigoVendas",
         pp.Name AS "NomeProduto",
         pp.ProductId AS "ProductId",
         p.Name AS "NomeCliente",
         s.CreatedAt AS "DataVenda",
-        spp.Value AS "ValorPago",
-        spp.Installments AS "QtdParcelas",
-        (spp.Value / spp.Installments) AS "ValorParcela",
-        pm.Name AS "FormaPagamento",
-        spp.Usn AS "NSU",
-        spp.AuthorizationCode AS "CodigoAutorizacao",
+        CASE
+          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
+        ORDER BY
+          s.SaleId) = 1 
+                  THEN spp.Value
+          ELSE NULL
+        END AS "ValorPago",
+        CASE
+          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
+        ORDER BY
+          s.SaleId) = 1 
+                  THEN spp.Installments
+          ELSE NULL
+        END AS "QtdParcelas",
+        CASE
+          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
+        ORDER BY
+          s.SaleId) = 1 
+                  THEN (spp.Value / spp.Installments)
+          ELSE NULL
+        END AS "ValorParcela",
+        CASE
+          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
+        ORDER BY
+          s.SaleId) = 1 
+                  THEN pm.Name
+          ELSE NULL
+        END AS "FormaPagamento",
+        CASE
+          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
+        ORDER BY
+          s.SaleId) = 1 
+                  THEN spp.Usn
+          ELSE NULL
+        END AS "NSU",
+        CASE
+          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
+        ORDER BY
+          s.SaleId) = 1 
+                  THEN spp.AuthorizationCode
+          ELSE NULL
+        END AS "CodigoAutorizacao",
         pt.ProductTypeId AS "CodigoCategoria",
         pt.Name AS "CategoriaProduto",
         p4.Partner AS "Parceiro"
-    FROM
+      FROM
         Sales s
-    INNER JOIN
-        SalesProducts sp
-    ON
+      LEFT JOIN 
+              SalesProducts sp ON
         s.SaleId = sp.SaleId
-    INNER JOIN
-        Products pp
-    ON
+      LEFT JOIN 
+              Products pp ON
         sp.ProductId = pp.ProductId
-    INNER JOIN
-        SalesPayments spp
-    ON
+      LEFT JOIN 
+              SalesPayments spp ON
         s.SaleId = spp.SaleId
-    INNER JOIN
-        Persons p
-    ON
+      LEFT JOIN 
+              Persons p ON
         s.PersonId = p.PersonId
-    INNER JOIN
-        PaymentMethods pm 
-    ON
+      LEFT JOIN 
+              PaymentMethods pm ON
         spp.PaymentMethodId = pm.PaymentMethodId
-    INNER JOIN
-        ProductTypes pt
-    ON
+      LEFT JOIN 
+              ProductTypes pt ON
         pp.ProductTypeId = pt.ProductTypeId
-    INNER JOIN
-      (
+      LEFT JOIN (
         SELECT
           p3.ProductId,
-            CASE
-              WHEN p3.Name LIKE '%Oficina Fabio Aquino%' THEN 'Oficina Fabio Aquino'
+          CASE
+            WHEN p3.Name LIKE '%Oficina Fabio Aquino%' THEN 'Oficina Fabio Aquino'
             WHEN p3.Name LIKE '%Surf Pool Training%' THEN 'Surf Pool Training'
             WHEN p3.Name LIKE '%Burle Experience%' THEN 'Burle Experience'
             WHEN p3.Name LIKE '%Camomile Massagem%' THEN 'Camomile Massagem'
             WHEN p3.Name LIKE '%Aprimore Surf%' THEN 'Aprimore Surf'
-            WHEN pt3.Name LIKE '%CT Surfland%' THEN 'CT Surfland'
+            WHEN p3.Name LIKE '%CT Surfland%' THEN 'CT Surfland'
             ELSE 'Outros'
-            END AS "Partner"
+          END AS "Partner"
         FROM
           Products p3
-        INNER JOIN
-          ProductTypes pt3
-        ON
-          p3.ProductTypeId = pt3.ProductTypeId
-      ) p4
-    ON
-      pp.ProductId = p4.ProductId
-    WHERE
-      pt.ProductTypeId not in (2, 6)
-      ${
-        paymentMethod
-          ? `AND pm.Name = '${paymentMethod}'`
-          : ""
-      }
-      ${
-        saleDate
-          ? `AND s.CreatedAt BETWEEN '${saleDate}' AND '${saleDateEnd}'`
-          : ""
-      }
-      ${
-        partnerName
-          ? `AND p4.Partner = '${partnerName}'`
-          : ""
-      }
+          ) p4 ON
+        pp.ProductId = p4.ProductId
+      WHERE
+        pt.ProductTypeId NOT IN (2, 6)
+        ${
+          paymentMethod ? `AND pm.Name = '${paymentMethod}'` : ''
+        }
+        ${
+          saleDate ? `AND s.CreatedAt BETWEEN '${saleDate}' AND '${saleDateEnd}'` : ''
+        }
+        ${
+          partnerName ? `AND p4.Partner = '${partnerName}'` : ''
+        }
     `;
 }
 
