c436d78b9d578b8ae1ccd7fa02e4c7e6ad7669b3
Daniel-Dief <danieldiefsantos@gmail.com>
2025-06-03 11:02:59 -0300

    feat: ajuste query queryPartnersReport

--- querys/partnerSalesReport.ts
+++ querys/partnerSalesReport.ts
@@ -7,107 +7,63 @@ interface queryPartnersReportProps {
 
 function queryPartnersReport({ paymentMethod, saleDate, saleDateEnd, partnerName }: queryPartnersReportProps) {
     return `
-      SELECT
-        s.SaleId AS "CodigoVendas",
-        pp.Name AS "NomeProduto",
-        pp.ProductId AS "ProductId",
-        p.Name AS "NomeCliente",
-        s.CreatedAt AS "DataVenda",
-        CASE
-          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
-        ORDER BY
-          s.SaleId) = 1 
-                  THEN spp.Value
-          ELSE NULL
-        END AS "ValorPago",
-        CASE
-          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
-        ORDER BY
-          s.SaleId) = 1 
-                  THEN spp.Installments
-          ELSE NULL
-        END AS "QtdParcelas",
-        CASE
-          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
-        ORDER BY
-          s.SaleId) = 1 
-                  THEN (spp.Value / spp.Installments)
-          ELSE NULL
-        END AS "ValorParcela",
-        CASE
-          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
-        ORDER BY
-          s.SaleId) = 1 
-                  THEN pm.Name
-          ELSE NULL
-        END AS "FormaPagamento",
-        CASE
-          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
-        ORDER BY
-          s.SaleId) = 1 
-                  THEN spp.Usn
-          ELSE NULL
-        END AS "NSU",
-        CASE
-          WHEN ROW_NUMBER() OVER (PARTITION BY s.SaleId
-        ORDER BY
-          s.SaleId) = 1 
-                  THEN spp.AuthorizationCode
-          ELSE NULL
-        END AS "CodigoAutorizacao",
-        pt.ProductTypeId AS "CodigoCategoria",
-        pt.Name AS "CategoriaProduto",
-        p4.Partner AS "Parceiro"
-      FROM
-        Sales s
-      LEFT JOIN 
-              SalesProducts sp ON
-        s.SaleId = sp.SaleId
-      LEFT JOIN 
-              Products pp ON
-        sp.ProductId = pp.ProductId
-      LEFT JOIN 
-              SalesPayments spp ON
-        s.SaleId = spp.SaleId
-      LEFT JOIN 
-              Persons p ON
-        s.PersonId = p.PersonId
-      LEFT JOIN 
-              PaymentMethods pm ON
-        spp.PaymentMethodId = pm.PaymentMethodId
-      LEFT JOIN 
-              ProductTypes pt ON
-        pp.ProductTypeId = pt.ProductTypeId
-      LEFT JOIN (
-        SELECT
-          p3.ProductId,
-          CASE
-            WHEN p3.Name LIKE '%Oficina Fabio Aquino%' THEN 'Oficina Fabio Aquino'
-            WHEN p3.Name LIKE '%Surf Pool Training%' THEN 'Surf Pool Training'
-            WHEN p3.Name LIKE '%Burle Experience%' THEN 'Burle Experience'
-            WHEN p3.Name LIKE '%Camomile Massagem%' THEN 'Camomile Massagem'
-            WHEN p3.Name LIKE '%Aprimore Surf%' THEN 'Aprimore Surf'
-            WHEN p3.Name LIKE '%CT Surfland%' THEN 'CT Surfland'
-            ELSE 'Outros'
-          END AS "Partner"
-        FROM
-          Products p3
-          ) p4 ON
-        pp.ProductId = p4.ProductId
-      WHERE
-        pt.ProductTypeId NOT IN (2, 6)
-        AND s.StatusId = 4
+      SELECT DISTINCT 
+		MOV.PK_ID AS CODIGOITEMVENDA,
+	    ped.pk_id AS codigoPedido,
+		tf.pk_id AS codigoFamilia,
+		tf.ds_familia AS familia,
+		Mov.fk_produto AS codigoProduto,
+		mov.DS_MODELO AS produto,
+		ped.nr_pedido AS numeroPedido,
+		CAST(ped.dt_pedido AS DATE) AS dataPedido,
+		mov.VL_PRETOT AS Valor,
+		MOV.QT_MOVIMENTO AS qtdItem,
+		PF.DS_DOCUMENTO AS formaPagamento,
+		case
+			when ped.fk_origemvenda = 'W' then PF.nr_nsu
+			else pf.DS_TEFNSUHOST 
+		end as NSU,
+		PF.ds_autorizacao,
+		ban.ds_bandeira AS bandeiraCartao,
+	  CAST(pc2.dh_catraca AS DATE) AS dataUtilizacao,
+		CAST(pc.DT_PREVISAOVISITA AS DATE) AS previsaoUso,
+		CASE
+			WHEN pc2.dh_catraca IS NULL
+			AND pc.DT_PREVISAOVISITA <= GETDATE() THEN 'No Show'
+			WHEN pc2.dh_catraca IS NULL
+			AND pc.DT_PREVISAOVISITA >= ped.dt_pedido THEN 'Uso Futuro'
+			ELSE 'Utilização na venda'
+		END AS statusUtilizacao,
+	case
+			when ped.fk_origemvenda = 'W' then 'Portal'
+			else 'Vixen' 
+		end as Origem
+	FROM vd_pedidos PED 
+	LEFT JOIN pv_financeiro pf ON PED.pk_id = PF.FK_ORIGEM
+	LEFT JOIN es_movimentos MOV ON PED.pk_id = MOV.FK_ORIGEM 
+	LEFT JOIN tb_produtos AS TP ON TP.pk_id = MOV.FK_PRODUTO
+	LEFT JOIN tb_familias AS TF ON TF.pk_id = TP.FK_FAMILIA
+	LEFT JOIN pv_tefbandeira AS BAN ON	BAN.cd_bandeira = PF.cd_tefbandeira
+	LEFT JOIN pq_ingresso AS PC ON PC.FK_ORIGEM = mov.FK_ORIGEM AND PC.FK_ORIGEMITEM = MOV.PK_ID 
+	LEFT JOIN pq_catraca AS PC2 ON PC.NR_PASSAPORTE = PC2.nr_passaporte AND PC.NR_PASSAPORTEDIG = PC2.NR_PASSAPORTEDIG
+	LEFT JOIN fi_movfin AS FM ON FM.PK_ID = PF.FK_MOVFIN 
+	WHERE
+		mov.TG_ORIGEM = 'V'
+		AND mov.tg_cancelado = 0
+		AND ped.dh_cancelado IS NULL
+		AND MOV.TG_INATIVO = 0
+		AND TF.pk_id in (43, 46, 49, 50, 52, 45)
+		and PED.fk_motivodevolucao = 0
+		${
+		 paymentMethod ? `AND pf.DS_DOCUMENTO LIKE '${paymentMethod}'` : ''
+	        }
+		${
+		  saleDate ? `AND ped.dt_pedido BETWEEN '${saleDate}' AND '${saleDateEnd}'` : ''
+		 }
         ${
-          paymentMethod ? `AND pm.Name = '${paymentMethod}'` : ''
-        }
-        ${
-          saleDate ? `AND s.CreatedAt BETWEEN '${saleDate}' AND '${saleDateEnd}'` : ''
-        }
-        ${
-          partnerName ? `AND p4.Partner = '${partnerName}'` : ''
-        }
-      ORDER BY
-        s.SaleId
+          partnerName ? `AND TF.pk_id = '${partnerName}'` : ''
+        }		
+	ORDER BY MOV.PK_ID 
     `;
 }
 
